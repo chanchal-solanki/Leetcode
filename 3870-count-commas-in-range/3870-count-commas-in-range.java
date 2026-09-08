d26707520b9f7eb931561c2b713a0689261a928f
Chanchal Solanki <chanchalsolanki1511@gmail.com>
2026-09-08 08:11:31 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,14 +1,6 @@
 class Solution {
     public int countCommas(int n) {
-        int n_copy = n;
-        int count = 0; 
-
-        while(n > 0) {
-            count ++ ;
-            n = n/10;
-        }
-       
-        if(count < 4) return 0;
-        return (n_copy - 1000) + 1 ;
+        if(n<1000) return 0;
+        return (n - 1000) + 1 ;
     }
 }
