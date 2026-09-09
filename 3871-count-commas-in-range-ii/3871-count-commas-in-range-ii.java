b5aa2e8f67453fb910416e96aa671abccde5770b
Chanchal Solanki <chanchalsolanki1511@gmail.com>
2026-09-09 11:07:13 +0530

    Time: 1 ms (99.36%), Space: 42.4 MB (85.26%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public long countCommas(long n) {
+        if(n<1000) return 0;
+        long res = 0; long start = 1000;
+
+        while(start <= n){
+            res += n-start+1;
+            start *= 1000;
+        }
+        return res;
+    }
+}
