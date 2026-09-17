b2c6130e0708a6bbe8f1669da95c296c74864ec7
Chanchal Solanki <chanchalsolanki1511@gmail.com>
2026-09-17 20:52:07 +0530

    Time: 4 ms (88%), Space: 46.7 MB (35.6%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -3,9 +3,11 @@ class Solution {
         if(numRows == 1 || s.length() == 1) return s;
         int n = s.length();
 
-        int[] index = new int[n];
+        StringBuilder[] sb = new StringBuilder[numRows];
         int row = -1;
         boolean down = true;
+
+        for(int i=0; i<numRows; i++) sb[i] = new StringBuilder();
 
         for(int i=0; i<n; i++){
             if(down){
@@ -16,19 +18,41 @@ class Solution {
                 row--;
                 if(row == 0) down = true;
             }
-            index[i] = row;
-            System.out.println(row);
+            sb[row].append(s.charAt(i));
         }
 
         StringBuilder ans = new StringBuilder();
-        row = 0;
-
-        while(row<numRows){
-            for(int i=0; i<n; i++){
-                if(row == index[i]) ans.append(s.charAt(i));
-            }
-            row++;
+        for(StringBuilder str : sb){
+            ans.append(str);
         }
         return ans.toString();
     }
 }
+
+        // int[] index = new int[n];
+        // int row = -1;
+        // boolean down = true;
+
+        // for(int i=0; i<n; i++){
+        //     if(down){
+        //         row++;
+        //         if(row == numRows-1) down = false;
+        //     }
+        //     else{
+        //         row--;
+        //         if(row == 0) down = true;
+        //     }
+        //     index[i] = row;
+        //     System.out.println(row);
+        // }
+
+        // StringBuilder ans = new StringBuilder();
+        // row = 0;
+
+        // while(row<numRows){
+        //     for(int i=0; i<n; i++){
+        //         if(row == index[i]) ans.append(s.charAt(i));
+        //     }
+        //     row++;
+        // }
+        // return ans.toString();
